1fa0365ceb67871afecfc31b94645e160dcbe5ff
rodric74 <rodric.rambosson@gmail.com>
2023-08-02 22:23:07 +0200

    mise en place serveur

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express'); // importer express avec require 
+
+const app = express(); // on appelle la méthode express
+
+module.exports = app; // on export cette constante pour y accéder depuis nos autres fichers. 
