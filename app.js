53d981f522fe0814335a580471038529b8ee9c50
rodric74 <rodric.rambosson@gmail.com>
2023-08-02 22:44:06 +0200

    ajout middleWare

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const express = require('express'); // importer express avec require 
+// importer express avec require 
+const express = require('express'); 
 
-const app = express(); // on appelle la méthode express
+// on appelle la méthode express
+const app = express(); 
 
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next(); // on appelle next pour aller au prochain middleware
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+
+// on export cette constante pour y accéder depuis nos autres fichers. 
 module.exports = app; // on export cette constante pour y accéder depuis nos autres fichers. 
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //importer le package http de node. 
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur mais avec Nodemon !');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000); //Par défaut on utilise le port 3000 on utilise process.env.PORT pour utiliser le port envoyé par le serveur sinon on utilise le port 3000. 
