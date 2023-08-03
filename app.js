531b8afee5995d6dc51204102c51f6360a1fd2ee
rodric74 <rodric.rambosson@gmail.com>
2023-08-03 09:09:07 +0200

    Construire API

--- app.js
+++ app.js
@@ -4,24 +4,36 @@ const express = require('express');
 // on appelle la méthode express
 const app = express(); 
 
+//Les middlewares:
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next(); // on appelle next pour aller au prochain middleware
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
 // on export cette constante pour y accéder depuis nos autres fichers. 
-module.exports = app; // on export cette constante pour y accéder depuis nos autres fichers. 
+module.exports = app; 
