f1f43284b8a1289381650af99610ec6d9be71600
rodric74 <rodric.rambosson@gmail.com>
2023-08-05 18:34:17 +0200

    fin du cours

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -26,5 +27,6 @@ app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing );
-router.put('/:id', stuffCtrl.modifyThings);
-router.delete('/:id', stuffCtrl.suppThings );
-router.get('/:id', stuffCtrl.getOneThing );
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllThings);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
-  module.exports = router;
-
+module.exports = router;
